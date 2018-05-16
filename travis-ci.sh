7fdf1504a971909347989372cd8ef4e7be48ed78
Allan Wang <me@allanwang.ca>
2018-05-15 23:06:43 -0700

    Update script

--- travis-ci.sh
+++ travis-ci.sh
@@ -2,4 +2,12 @@
 
 # Note that travis_ci_test db should be created by now
 
+printf "Running travis ci script\n"
+
 touch priv.properties
+echo "TEST_DB=jdbc:postgresql:travis_ci_test" >> priv.properties
+echo "TEST_DRIVER=org.postgresql.Driver" >> priv.properties
+echo "TEST_DB_USER=postgres" >> priv.properties
+
+chmod +x ./gradlew
+./gradlew test
